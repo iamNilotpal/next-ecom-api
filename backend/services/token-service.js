232b56b31e53f3d09205510465aa8b64c39aba41
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-07-21 17:52:42 +0530

    Add service for generating, validating, storing in database, setting in cookie of access and refresh tokens.

---
+++ backend/services/token-service.js
@@ -0,0 +1,84 @@
+const util = require('util');
+const JWT = require('jsonwebtoken');
+const RefreshToken = require('../models/token-model');
+const { createVerify } = require('crypto');
+
+const signToken = util.promisify(JWT.sign);
+const verifyToken = util.promisify(JWT.verify);
+
+const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET;
+const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET;
+
+class TokenService {
+  async accessToken(payload) {
+    return signToken(payload, JWT_ACCESS_TOKEN_SECRET, {
+      expiresIn: '1h',
+      issuer: process.env.BASE_URL,
+      audience: String(payload.id),
+    });
+  }
+
+  async refreshToken(payload) {
+    try {
+      const refreshToken = await signToken(payload, JWT_REFRESH_TOKEN_SECRET, {
+        expiresIn: '1y',
+        issuer: process.env.BASE_URL,
+        audience: String(payload.id),
+      });
+      await this.storeRefreshToken(refreshToken, payload.id);
+      return refreshToken;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async storeRefreshToken(token, userId) {
+    return RefreshToken.create({ token, userId });
+  }
+
+  setTokenCookie(res, key, data) {
+    res.cookie(key, data.token, {
+      maxAge: data.age,
+      httpOnly: true,
+      sameSite: 'none',
+      path: '/',
+      secure: true,
+    });
+  }
+
+  setAccessToken(res, token) {
+    this.setTokenCookie(res, 'accessToken', {
+      token,
+      age: 1000 * 60 * 60 * 1,
+    });
+  }
+
+  setRefreshToken(res, token) {
+    this.setTokenCookie(res, 'refreshToken', {
+      token,
+      age: 1000 * 60 * 60 * 24 * 365,
+    });
+  }
+
+  clearCookie(res, key) {
+    res.clearCookie(key);
+  }
+
+  async verifyAccessToken(token) {
+    return verifyToken(token, JWT_ACCESS_TOKEN_SECRET);
+  }
+
+  async verifyRefreshToken(token) {
+    return verifyToken(token, JWT_REFRESH_TOKEN_SECRET);
+  }
+
+  async findRefreshToken(token, userId) {
+    return RefreshToken.findOne({ token, userId });
+  }
+
+  async deleteRefreshToken(token) {
+    return RefreshToken.deleteOne({ token });
+  }
+}
+
+module.exports = new TokenService();
