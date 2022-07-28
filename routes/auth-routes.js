96582399d89f35c270baec24d19f7c8c1ce67d03
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-07-29 01:55:37 +0530

    Fixed import authMiddleware

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
